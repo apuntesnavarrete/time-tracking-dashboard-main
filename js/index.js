8b1e3bd6ee79301ecbdc2524ce6258ad5b4fa467
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-10-07 00:49:49 -0500

    javascript detecta el input pulsado

--- js/index.js
+++ js/index.js
@@ -1,13 +1,36 @@
 let input = document.getElementsByTagName("input")
 let date = document.getElementsByClassName("card_principal-date")[0]
+let current = document.getElementsByClassName("info-hours")
+
+current[0].innerHTML = "32hrs"
+current[1].innerHTML = "10hrs"
+current[2].innerHTML = "4hrs"
+
+
+
+console.log(current)
 
 console.log(date)
 
 date.addEventListener("change", function(){
     for (item of input) {
-        console.log(item);
 
-        console.log(item.checked);
+        if(item.checked == true){
+
+            if(item.value == "Daily"){
+                console.log("acciones dayli")
+            }
+
+            if(item.value == "Weekly"){
+                console.log("acciones weekly")
+            }
+
+            if(item.value == "Monthly"){
+                console.log("acciones mon")
+            }
+        }
+
+        
     }}
     )
 

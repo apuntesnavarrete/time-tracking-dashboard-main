507f15d0866d044432abef344a216368fdcf288f
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-10-07 01:24:57 -0500

    javascript funcionando insercion de datos desde json

--- js/index.js
+++ js/index.js
@@ -2,38 +2,78 @@ let input = document.getElementsByTagName("input")
 let date = document.getElementsByClassName("card_principal-date")[0]
 let current = document.getElementsByClassName("info-hours")
 
-current[0].innerHTML = "32hrs"
-current[1].innerHTML = "10hrs"
-current[2].innerHTML = "4hrs"
 
 
 
-console.log(current)
+fetch('https://apuntesnavarrete.github.io/time-tracking-dashboard-main/data.json')
+  .then(response => response.json())
+  .then(data => {
 
-console.log(date)
+    for (let i = 0; i < current.length; i++) {
+        current[i].innerHTML = data[i].timeframes.weekly.current + "hrs"
 
-date.addEventListener("change", function(){
+    }
+   
+
+  date.addEventListener("change", function(){
     for (item of input) {
 
         if(item.checked == true){
 
             if(item.value == "Daily"){
+                for (let i = 0; i < current.length; i++) {
+                    current[i].innerHTML = data[i].timeframes.daily.current + "hrs"
+
+                }
+
                 console.log("acciones dayli")
             }
 
             if(item.value == "Weekly"){
+
+                for (let i = 0; i < current.length; i++) {
+                    current[i].innerHTML = data[i].timeframes.weekly.current + "hrs"
+
+                }
+
+
                 console.log("acciones weekly")
             }
 
             if(item.value == "Monthly"){
+                for (let i = 0; i < current.length; i++) {
+                    current[i].innerHTML = data[i].timeframes.monthly.current + "hrs"
+
+                }
                 console.log("acciones mon")
             }
         }
 
         
-    }}
-    )
+    }
+  })
+    
 
+    for (let i = 0; i < data.length; i++) {
+        console.log(`ciclo card numero.- ${i}`)
+        console.log(data[i].title)
+        console.log(data[i].timeframes)
+        console.log(data[i].timeframes.daily)
+        console.log(data[i].timeframes.daily.current)
+        console.log(data[i].timeframes.daily.previous)
+    }
+
+
+
+  });
+
+
+
+
+
+    function checarInput(){
+       
+    }
 
 
 
